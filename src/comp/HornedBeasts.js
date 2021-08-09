1e0b20a26142b7270124ce8622bab1a1effe4828
maiada-ibrahim <maiada.ibrahim.27@icloud.com>
2021-08-09 16:30:52 +0300

    finish

--- src/comp/HornedBeasts.js
+++ src/comp/HornedBeasts.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import Card from 'react-bootstrap/Card'
-import Button from 'react-bootstrap/Button'
+import Card from 'react-bootstrap/Card';
+// import Button from 'react-bootstrap/Button';
 // import {Card,Button} from 'react-bootstrap/';
 
 
@@ -21,11 +21,22 @@ class HornedBeasts extends React.Component {
   render() {
     return (
 
-      <div>  <h2>{this.props.title}</h2>
-        <img onClick={this.increaseNuOfPets}  src={this.props.imageUrl} alt='h' title='h' />
-        <p>{this.props.description}</p>
-        <p> ❤️favorited = {this.state.numberOfPets}</p>
-        <br /><br /><br /><br /><br /><br /> </div>
+      // <div>  <h2>{this.props.title}</h2>
+      //   <img onClick={this.increaseNuOfPets}  src={this.props.imageUrl} alt='h' title='h' />
+      //   <p>{this.props.description}</p>
+      //   <p> ❤️favorited = {this.state.numberOfPets}</p>
+      //   <br /><br /><br /><br /><br /><br /> </div>
+      <Card style={{ width: '18rem' }}>
+        <Card.Img variant="top" onClick={this.increaseNuOfPets} src={this.props.imageUrl} alt='h' title='h' />
+        <Card.Body>
+          <Card.Title>{this.props.title}</Card.Title>
+          <Card.Text>
+            {this.props.description}
+            ❤️favorited = {this.state.numberOfPets}
+          </Card.Text>
+          {/* <Button variant="primary">Go somewhere</Button> */}
+        </Card.Body>
+      </Card>
     );
 
   }
--- src/comp/Main.js
+++ src/comp/Main.js
@@ -1,24 +1,26 @@
 import React from 'react';
 import HornedBeasts from './HornedBeasts';
+import arrayanimal from './data.json';
 
-let arrayanimal = [{
-  title: "UniWhal",
-  description: "A unicorn and a narwhal nuzzling their horns",
-  imageUrl: "http://3.bp.blogspot.com/_DBYF1AdFaHw/TE-f0cDQ24I/AAAAAAAACZg/l-FdTZ6M7z8/s1600/Unicorn_and_Narwhal_by_dinglehopper.jpg",
-},
+// let arrayanimal = [{
+//   title: "UniWhal",
+//   description: "A unicorn and a narwhal nuzzling their horns",
+//   imageUrl: "http://3.bp.blogspot.com/_DBYF1AdFaHw/TE-f0cDQ24I/AAAAAAAACZg/l-FdTZ6M7z8/s1600/Unicorn_and_Narwhal_by_dinglehopper.jpg",
+// },
 
-{
-  title: "Rhino Family",
-  description: "Mother (or father) rhino with two babies",
-  imageUrl: "https://images.unsplash.com/photo-1512636618879-bbe79107e9e3?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=bd9460ee6d1ddbb6b1ca7be86dfc4590&auto=format&fit=crop&w=1825&q=80.jpg"
-},
+// {
+//   title: "Rhino Family",
+//   description: "Mother (or father) rhino with two babies",
+//   imageUrl: "https://images.unsplash.com/photo-1512636618879-bbe79107e9e3?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=bd9460ee6d1ddbb6b1ca7be86dfc4590&auto=format&fit=crop&w=1825&q=80.jpg"
+// },
 
 
-{
-  title: "Unicorn Head",
-  description: "Someone wearing a creepy unicorn head mask",
-  imageUrl: "https://www.dhresource.com/0x0s/f2-albu-g5-M00-1A-11-rBVaI1hsIIiALxKzAAIHjSU3VkE490.jpg/wholesale-halloween-costume-prop-unicorn.jpg"
-},];
+// {
+//   title: "Unicorn Head",
+//   description: "Someone wearing a creepy unicorn head mask",
+//   imageUrl: "https://www.dhresource.com/0x0s/f2-albu-g5-M00-1A-11-rBVaI1hsIIiALxKzAAIHjSU3VkE490.jpg/wholesale-halloween-costume-prop-unicorn.jpg"
+// },];
+
 
 class Main extends React.Component {
 
@@ -36,7 +38,7 @@ class Main extends React.Component {
             < HornedBeasts
               title={item.title}
               description={item.description}
-              imageUrl={item.imageUrl} />);
+              imageUrl={item.image_url} />);
 
         })}
 
