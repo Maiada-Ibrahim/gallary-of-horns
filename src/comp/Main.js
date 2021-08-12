f3d3022cd593f78b16cfd660b96f477896eb1544
maiada-ibrahim <maiada.ibrahim.27@icloud.com>
2021-08-12 09:33:47 +0300

    rendernotwork

--- src/comp/Main.js
+++ src/comp/Main.js
@@ -47,7 +47,10 @@ class Main extends React.Component {
   };
   
   
-  
+  renderallfun = () => {
+   this.props.onchangefun()
+   this.rendernowfun()
+  };
   
   
   render() {
@@ -55,7 +58,7 @@ class Main extends React.Component {
 
       <div>
         
-        {this.rendernowfun}
+        {this.renderallfun}
 
         {/* < HornedBeasts title="UniWhal" description="A unicorn and a narwhal nuzzling their horns" imageUrl="http://3.bp.blogspot.com/_DBYF1AdFaHw/TE-f0cDQ24I/AAAAAAAACZg/l-FdTZ6M7z8/s1600/Unicorn_and_Narwhal_by_dinglehopper.jpg" />
         < HornedBeasts title="Rhino Family" description="Mother (or father) rhino with two babies" imageUrl="https://images.unsplash.com/photo-1512636618879-bbe79107e9e3?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=bd9460ee6d1ddbb6b1ca7be86dfc4590&auto=format&fit=crop&w=1825&q=80.jpg" />
