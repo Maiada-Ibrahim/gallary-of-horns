204441b1bf22138f3087da2494bfb5b0c4ff8a03
maiada-ibrahim <maiada.ibrahim.27@icloud.com>
2021-08-12 13:05:25 +0300

    finsh lab4without css

--- src/App.js
+++ src/App.js
@@ -3,7 +3,6 @@ import Header from './comp/Header';
 import Footer from './comp/Footer';
 import Main from './comp/Main';
 // import Main from './comp/SelectedBeast ';
-import FormDesgin from './comp/FormDesgin';
 import arrayanimal from './comp/data.json';
 
 
@@ -25,37 +24,15 @@ class App extends React.Component {
   // },
   // );
 
-  onchangefun = async (event) => {
-    event.preventDefault();
-
-    let newarray=[];
-    let arrayafterfillter1 =await this.state.arrayanimal.filter(value => {
-
-      if (value.horns === Number(event.target.value) ) { 
-        newarray.push(value);
-        return 1;
-      }
-      
-
-    });
-    
-    // console.log(arrayafterfillter1);
-    
-    this.setState({
-      arrayafterfillter: this.state.arrayafterfillter = arrayafterfillter1});
-    console.log(this.state.arrayafterfillter);
-    return arrayafterfillter1;
-  };
+ 
   render() {
     return (
 
       <>
 
         <Header />
-        <FormDesgin onchangefun={this.onchangefun}
-          arrayafterfillter= {this.state.arrayafterfillter}
-        />
-        <Main  arrayafterfillter= {this.state.arrayafterfillter} onchangefun={this.onchangefun}/>
+        
+        <Main arrayafterfillter={this.state.arrayafterfillter} onchangefun={this.onchangefun} />
         <Footer />
         <Footer />
 
--- src/comp/FormDesgin.js
+++ src/comp/FormDesgin.js
@@ -10,13 +10,14 @@ class FormDesgin extends React.Component {
       <div>
         <Form.Label>what is num of horrn?</Form.Label>
         <Form.Select aria-label="Default select example" name='form' onChange={this.props.onchangefun} >
-          <option value= "1">one</option>
+          <option value="5">all</option>
+          <option value="1">one</option>
           <option value="2">Two</option>
           <option value="3">Three</option>
           <option value="100">woow</option>
 
         </Form.Select>
-       
+
       </div>
 
     );
